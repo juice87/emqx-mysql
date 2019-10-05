3f40f009337b1cb6610253b686be91f289ee1e1e
juice87 <35162913+juice87@users.noreply.github.com>
2019-10-05 23:09:32 +0800

    Update emqx_mysql.erl

--- src/emqx_mysql.erl
+++ src/emqx_mysql.erl
@@ -51,8 +51,10 @@ on_message_publish(#message{flags = #{retain := true}} = Message, _State) ->
 	Z_QDFLAG =  maps:get(<<"Z_QDFLAG">>, MapData),
 	Z_QDPLSTIM =  maps:get(<<"Z_QDPLSTIM">>, MapData),
 	Z_QDTEMPZ05 =  maps:get(<<"Z_QDTEMPZ05">>, MapData),	
-			
-	emqx_mysql_cli:query(?SAVE_MESSAGE_PUBLISH, [emqx_guid:to_hexstr(Id), binary_to_list(From), binary_to_list(Topic), Z_QDMLDCLSTIM, timestamp()]),
+	
+	emqx_mysql_cli:query(?SAVE_CYCLEDATA,[binary_to_list(From), timestamp(), ControllerId, Z_QDVPPOS, Z_QDPRDCNT, Z_QDCOLTIM, Z_QDMAXPLSRPM, Z_QDCYCTIM, Z_QDINJTIM, Z_QDMLDCLSTIM, Z_QDTEMPZ01, Z_QDTEMPZ02, Z_QDTEMPZ03, Z_QDTEMPZ04, Z_QDTEMPZ05, Z_QDTEMPZ06, Z_QDNOZTEMP, Z_QDGODCNT, Z_QDHLDTIM, Z_QDBCKPRS, Z_QDMLDOPNENDPOS, Z_QDMAXINJSPD, Z_QDINJENDPOS, Z_QDPLSENDPOS, Z_QDMLDOPNTIM, Z_QDPLSTIM, Z_QDFLAG, EventTime]),
+	
+	%%emqx_mysql_cli:query(?SAVE_MESSAGE_PUBLISH, [emqx_guid:to_hexstr(Id), binary_to_list(From), binary_to_list(Topic), Z_QDMLDCLSTIM, timestamp()]),
 	%%emqx_mysql_cli:query(?SAVE_MESSAGE_PUBLISH, [emqx_guid:to_hexstr(Id), binary_to_list(From), binary_to_list(Topic), binary_to_list(Payload), timestamp()]),
 	{ok, Message};
 on_message_publish(Message, _State) ->
